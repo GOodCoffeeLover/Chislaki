02a30371919b4841bbd916a2d2bc544de168d7c2
student31415 <qwerty65k@mail.ru>
2021-04-02 15:46:23 +0300

    made it better

--- 1lab/Yakobi.m
+++ 1lab/Yakobi.m
@@ -1,8 +1,6 @@
 format compact
-A = [-4, -6, -10; 
-    -6, -11,  4;
-    -10, 4, -3];
-  [L,V] = yakobi(A, 6)
+
+  
   function [k, m] = maxij(A)
     k = 1;
     m = 2;
@@ -19,15 +17,23 @@ A = [-4, -6, -10;
   function [A, V] = yakobi(A, n)
     V = eye(size(A));
     for i = 1:n
+        i
         [k,m] = maxij(A);
-        fi = atan(2*A(k,m)/(A(k,k) - A(m,m)))/2;
+        fi = atan(2*A(k,m)/(A(k,k) - A(m,m)))/2
         h=eye(size(A));
         h(k,k)= cos(fi);  h(k,m) = -sin(fi);
         h(m,k)= sin(fi);  h(m,m) = cos(fi);
         h
+        inv(h)*A
         V = V*h;
         A= inv(h)*A*h
     end
+    for i = 1:size(V)
+        V(:,i)/=max(V(:, i));
+    end
  end
  
- 
+ A = [-4, -6, -10; 
+    -6, -11,  4;
+    -10, 4, -3];
+[L,V] = yakobi(A, 6)
